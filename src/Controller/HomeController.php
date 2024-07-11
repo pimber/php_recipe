6a1461582fe50936c94933483d8975a68451aeef
Christoffer T Jønsson <christofferctj@gmail.com>
2024-07-11 22:30:33 +0200

    Added all the html and css for all the different pages. Added two controllers for home and account

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,24 @@ class HomeController extends AbstractController
     #[Route('/', name:'home')]
     public function homepage(): Response
     {
-        return $this->render('homepage.html.twig');
+        return $this->render('pages/homepage.html.twig');
+    }
+
+    #[Route('/recipes', name:'recipes')]
+    public function recipes(): Response
+    {
+        return $this->render('pages/recipes.html.twig');
+    }
+
+    #[Route('/contact', name:'contact')]
+    public function contact(): Response
+    {
+        return $this->render('pages/contact.html.twig');
+    }
+
+    #[Route('/myrecipes', name:'myrecipes')]
+    public function myrecipes(): Response
+    {
+        return $this->render('pages/myrecipes.html.twig');
     }
 }
