6da0816264c776f89479a03a38f847fc7104a083
Christoffer T Jønsson <christofferctj@gmail.com>
2024-07-15 14:57:47 +0200

    Small frontend changes and added truncate functions

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('truncateString', [$this, 'truncateString']),
+        ];
+    }
+
+    public function truncateString($string, $length)
+    {
+        if (mb_strlen($string, 'UTF-8') > $length) {
+            $string = mb_substr($string, 0, $length, 'UTF-8');
+        }
+        return $string;
+    }
+}
